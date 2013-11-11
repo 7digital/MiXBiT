2a543fb4389986101f09eba6629fe49c065235b5
Raoul Millais <raoul@raoulmillais.com>
2013-11-11 19:50:01 +0000

    Make robot more controllable from commandline

--- robot.js
+++ robot.js
@@ -4,8 +4,54 @@ var _ = require('lodash');
 var rooms = require('./lib/rooms');
 var robotDj = require('./lib/robot-dj');
 
-rooms.all(function (err, rooms) {
-	_.each(rooms, function monitorRoom(room) {
-		robotDj.monitor(room, process.send);
+if (process.argv.length <= 2) {
+	return monitorAll();
+}
+
+function die(msg) {
+	console.err(msg);
+	process.exit(1);
+}
+
+switch (process.argv[2]) {
+	case 'init':
+		return initRooms();
+	case 'monitor':
+		return monitorOne();
+	case 'monitor-all':
+		return monitorAll();
+	default:
+		return die('Unrecognised command');
+}
+
+function monitorAll() {
+	rooms.all(function (err, rooms) {
+		_.each(rooms, function monitorRoom(room) {
+			console.log('Monitoring room %s', room.name);
+			robotDj.monitor(room, function (room) {
+				console.log('******* NOTIFIED **********');
+			});
+		});
 	});
-});
+}
+
+function monitorOne() {
+	if (process.argv.length < 4) {
+		return die('Missing room id');
+	}
+	rooms.get(process.argv[3], function (err, room) {
+		if (err || !room) {
+			return die(err || 'Could not find room');
+		}
+
+		robotDj.monitor(room, function (room) {
+			console.log('******* NOTIFIED **********');
+		});
+	});
+}
+
+function initRooms() {
+	rooms.init(function initComplete(err) {
+		console.log('Initialised some rooms');
+	});
+}
